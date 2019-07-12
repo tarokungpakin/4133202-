96f7a8cc2b25d1bc4396c1273c188a5e39c72449
tarokungpakin <taroza2542@hotmail.com>
2019-07-12 15:34:29 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,18 @@
+<?php
+ echo $_POST['user'];
+ 
+ echo "Hello World !!!";
+
+ $name = "Taro";
+ echo "Mr." .$name;
+
+
+ echo sum(5,3);
+ //function
+ function sum($A ,$B){
+     return $A + $B;
+ }
+?>
+
+<!--shot term use one line only. -->
+<?='My name is Taro'?>
